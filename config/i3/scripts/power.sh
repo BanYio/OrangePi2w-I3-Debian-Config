69670528a315675caf65a0d2f744e8ff9ad103b2
Alejandro Fernandez <154221253+BanYio@users.noreply.github.com>
2025-12-19 13:52:59 +0100

    Update power.sh

--- config/i3/scripts/power.sh
+++ config/i3/scripts/power.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-CHOICE=$(printf "  Lock\n  Logout\n  Reboot\n⏻  Poweroff" | \
+CHOICE=$(printf "  Logout\n  Reboot\n⏻  Poweroff" | \
   rofi -dmenu -p "Power" -theme ~/.config/rofi/power.rasi)
 
 case "$CHOICE" in
-  *Lock*)    i3lock ;;
   *Logout*)  i3-msg exit ;;
   *Reboot*)  systemctl reboot ;;
   *Poweroff*) systemctl poweroff ;;
